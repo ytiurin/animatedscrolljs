f965a0850ab67ae3242e730a44a113876e0e73bc
unknown <etyurin@Sapphire2.ksf>
2013-10-04 11:14:55 +0300

    Release 1.1.4

---
+++ jquery.animatedscroll-1.1.4.min.js
@@ -0,0 +1,16 @@
+/**
+* AnimatedScroll.js v1.1.4
+* Smooth, animated document scroll to a specific element, supporting native jQuery UI easings.
+* https://github.com/yevhentiurin/animatedscrolljs
+*
+* By Yevhen Tiurin <yevhentiurin@gmail.com>, Mikhail Semakhin <mike.semakhin@gmail.com>, Carlo Alberto Ferraris <cafxx@cafxx.strayorange.com>
+* Licensed under the LGPL Version 3 license.
+* http://www.gnu.org/licenses/lgpl.txt
+*
+* Packed with Dean Edwards JavaScript Compressor version 3.0
+* http://dean.edwards.name/packer/
+*
+* Date: October 4, 2013
+**/
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(3($){$.i={y:{},z:{8:"A%",9:"A%"}};$.P.i=3(a,b){a=$.p({},$.i.y,a);b=$.p({},$.i.z,b);B(6.Q(0),a,b);j 6};3 B(d,e,f){C g,7,q,r,k,l,s,t,4,5,u,v,m,n;g=o.R-D;7=o.S-D;q=$(d).T();r=$(d).U();k=$(w).E();l=$(w).F();s=$(d).G().8;t=$(d).G().9;3 x(a,b){C c=V(b);h(W(c))j 0;h(b.H!==X){h(b.H("%")>-1){j(a*c/Y)}};j c};u=x(q,f.8);v=x(r,f.9);4=s+u-(g/2);4=4<0?0:(4+g>k?k-g:4);5=t+v-(7/2);5=5<0?0:(5+7>l?l-7:5);m=e.I;n=e.J;e=$.p({},e,{I:3(a,b){b.Z.K(L);h(M m=="3"){m.N(6,O)}},J:3(){6.K(L);$(6).10();h(M n=="3"){n.N(6,O)}}});$("<11/>").12({13:\'14\',15:"16",E:g,F:7,8:$(o).17(),9:$(o).18()}).19(w.1a).1b({8:4,9:5},e)}})(1c);',62,75,'|||function|animateLeft|animateTop|this|viewportHeight|left|top||||||||if|animatedScroll|return|documentWidth|documentHeight|animateStep|animateComplete|window|extend|targetWidth|targetHeight|targetLeft|targetTop|offsetLeft|offsetTop|document|parseOffsetValue|animateOptions|offsetFromTarget|50|AnimatedScroll|var|20|width|height|offset|indexOf|step|complete|scrollIntoView|true|typeof|apply|arguments|fn|get|innerWidth|innerHeight|outerWidth|outerHeight|parseInt|isNaN|undefined|100|elem|remove|div|css|visibility|hidden|position|absolute|scrollLeft|scrollTop|appendTo|body|animate|jQuery'.split('|'),0,{}))
