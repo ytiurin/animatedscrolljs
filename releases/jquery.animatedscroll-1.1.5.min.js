e91dce20c610807d992c1907aea3e9860a749fda
unknown <etyurin@Sapphire2.ksf>
2013-12-02 14:40:57 +0200

    Release 1.1.5

---
+++ releases/jquery.animatedscroll-1.1.5.min.js
@@ -0,0 +1,16 @@
+/**
+* AnimatedScroll.js v1.1.5
+* Smooth, animated document scroll to a specific element, supporting native jQuery UI easings.
+* https://github.com/yevhentiurin/animatedscrolljs
+*
+* By Yevhen Tiurin <yevhentiurin@gmail.com>, Mikhail Semakhin <mike.semakhin@gmail.com>, Carlo Alberto Ferraris <cafxx@cafxx.strayorange.com>
+* Licensed under the LGPL Version 3 license.
+* http://www.gnu.org/licenses/lgpl.txt
+*
+* Packed with Dean Edwards JavaScript Compressor version 3.0
+* http://dean.edwards.name/packer/
+*
+* Date: December 2, 2013
+**/
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(3($){$.k={C:{},D:{9:"E%",h:"E%"}};$.Y.k=3(a,b){a=$.t({},$.k.C,a);b=$.t({},$.k.D,b);F(6.Z(0),a,b);l 6};3 F(d,e,f){G g,7,8,u,v,m,n,w,x,4,5,y,z,o,p,$i;$i=$(\'<H 10="I:J;K:11;q:A%;r:A%" />\').L(s.M);7=$i.q();8=$i.r();$i.N();u=$(d).12();v=$(d).13();m=$(s).q();n=$(s).r();w=$(d).O().9;x=$(d).O().h;3 B(a,b){G c=14(b);j(15(c))l 0;j(b.P!==16){j(b.P("%")>-1){l(a*c/A)}};l c};y=B(u,f.9);z=B(v,f.h);4=w+y-(7/2);4=4<0?0:(4+7>m?m-7:4);5=x+z-(8/2);5=5<0?0:(5+8>n?n-8:5);o=e.Q;p=e.R;e=$.t({},e,{Q:3(a,b){b.17.S(T);j(U o=="3"){o.V(6,W)}},R:3(){6.S(T);$(6).N();j(U p=="3"){p.V(6,W)}}});$("<H/>").18({I:\'J\',K:"19",q:7,r:8,9:$(X).1a(),h:$(X).1b()}).L(s.M).1c({9:4,h:5},e)}})(1d);',62,76,'|||function|animateLeft|animateTop|this|viewportWidth|viewportHeight|left||||||||top|viewportDetectElement|if|animatedScroll|return|documentWidth|documentHeight|animateStep|animateComplete|width|height|document|extend|targetWidth|targetHeight|targetLeft|targetTop|offsetLeft|offsetTop|100|parseOffsetValue|animateOptions|offsetFromTarget|50|AnimatedScroll|var|div|visibility|hidden|position|appendTo|body|remove|offset|indexOf|step|complete|scrollIntoView|true|typeof|apply|arguments|window|fn|get|style|fixed|outerWidth|outerHeight|parseInt|isNaN|undefined|elem|css|absolute|scrollLeft|scrollTop|animate|jQuery'.split('|'),0,{}))
